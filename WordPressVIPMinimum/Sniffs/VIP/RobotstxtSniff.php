195a1b7f3cd64fa53e00b386b064e46e9c10b5b5
david-binda <david.binovec@gmail.com>
2017-02-13 20:13:36 +0000

    Flag `do_robotstxt` action callback
    Since we need to flush NginX cache in production in order to make the change taking effect after the code is deployed, we should remind this to deployers and PHPCS is an easy enough path for surfacing this information.
    
    Fixes #25

---
+++ WordPressVIPMinimum/Sniffs/VIP/RobotstxtSniff.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This sniff searches for `do_robotstxt` action hooked callback and thows an internal reminder
+ * for VIP devs to flush related caches in order to make the change actually happen in production
+ */
+class WordPressVIPminimum_Sniffs_VIP_RobotstxtSniff implements PHP_CodeSniffer_Sniff {
+	
+	/**
+	 * Returns the token types that this sniff is interested in.
+	 *
+	 * @return array(int)
+	 */
+	public function register() {
+		return PHP_CodeSniffer_Tokens::$functionNameTokens;
+	}// end register()
+
+	/**
+	 * Processes the tokens that this sniff is interested in.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile The file where the token was found.
+	 * @param int $stackPtr  The position in the stack where the token was found.
+	 *
+	 * @return void
+	 */
+	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
+		
+		$tokens = $phpcsFile->getTokens();
+	
+		$functionName = $tokens[$stackPtr]['content'];
+
+		if ( 'add_action' !== $functionName ) {
+			return;
+		}
+
+		$actionNamePtr = $phpcsFile->findNext(
+			array_merge( PHP_CodeSniffer_Tokens::$emptyTokens, array( T_OPEN_PARENTHESIS ) ), //types
+			$stackPtr + 1, //start
+			null, //end
+			true, //exclude
+			null, //value,
+			true //local
+		);
+
+		if ( ! $actionNamePtr ) {
+			// Something is wrong
+			return;
+		}
+
+		if ( 'do_robotstxt' === substr( $tokens[$actionNamePtr]['content'], 1, -1 ) ) {
+			$phpcsFile->addWarning( 'Internal note: remember to flush robots.txt nginx cache after deploying this revision', $stackPtr );
+		}
+
+	}// end process()
+
+}
