530283b0bd26ccc23ff4cac32aa19d56639b2a70
David Binovec <david.binovec@gmail.com>
2017-04-18 10:52:30 +0000

    Adding tests for ErrorControl sniff

---
+++ WordPressVIPMinimum/Tests/VIP/ErrorControlUnitTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Unit test class for WordPressVIPMinimum Coding Standard.
+ */
+
+/**
+ * Unit test class for the AdminBarRemoval sniff.
+ */
+class WordPressVIPMinimum_Tests_VIP_ErrorControlUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array(
+			3 => 1,
+		);
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array(
+		);
+
+	}
+
+} // End class.
