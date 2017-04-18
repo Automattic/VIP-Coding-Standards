79b248b98177e9c426cc149720d54e8b1770edc5
David Binovec <david.binovec@gmail.com>
2017-04-18 14:08:07 +0000

    Adding RestrictedFunctions sniff's unit test

---
+++ WordPressVIPMinimum/Tests/VIP/RestrictedFunctionsUnitTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Unit test class for WordPressVIPMinimum Coding Standard.
+ */
+
+/**
+ * Unit test class for the high remote request timeout sniff.
+ */
+class WordPressVIPMinimum_Tests_VIP_RestrictedFunctionsUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array(
+			3 => 1,
+			5 => 1,
+			7 => 1,
+			9 => 1,
+			11 => 1,
+		);
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array();
+
+	}
+
+} // End class.
