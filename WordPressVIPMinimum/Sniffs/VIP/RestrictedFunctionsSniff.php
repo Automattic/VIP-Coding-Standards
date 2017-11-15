ec54dc90c06cc7ab55e4f535981acd12f4c4dc74
David Binovec <david.binovec@gmail.com>
2017-11-15 14:32:55 +0000

    The upstream repository removed the deprecated wpcom helpers so our restricted functions Sniff is producing a Warning now.
    This commit addresses that by not relying on those being present. The PHPCS message produced by the sniff has been simplified and thus a bunch of helper methods could had been removed.

--- WordPressVIPMinimum/Sniffs/VIP/RestrictedFunctionsSniff.php
+++ WordPressVIPMinimum/Sniffs/VIP/RestrictedFunctionsSniff.php
@@ -72,7 +72,7 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 		foreach ( $deprecated_vip_helpers as $restricted => $helper ) {
 			$new_groups[ $helper ] = array(
 				'type'      => 'warning',
-				'message'   => '%s() is deprecated, please use ' . $this->get_alternatives( $original_groups[ $restricted ]['functions'] ) . ' instead.',
+				'message'   => "%s() is deprecated, please use {$restricted} instead.",
 				'functions' => array(
 					$helper,
 				),
@@ -83,47 +83,4 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 		return array_merge( $original_groups, $new_groups );
 
 	} // end getGroups().
-
-	/**
-	 * Return formatted list of alternative functions.
-	 *
-	 * @param array $function_list List of alternative functions.
-	 *
-	 * @return string Formatted list of alternative functions.
-	 */
-	private function get_alternatives( $function_list ) {
-		$function_list = array_map( array( $this, 'append_brackets' ), $function_list );
-		return $this->join_with_comma_and_or( $function_list );
-	}
-
-	/**
-	 * Append brackets "()" to the function name.
-	 *
-	 * @param string $item A function name w/o trailing brackets.
-	 *
-	 * @return string Function name with trailing brackets().
-	 */
-	private function append_brackets( $item ) {
-		return $item . '()';
-	}
-
-	/**
-	 * Return grammatically correct list of alternatives.
-	 *
-	 * The method accepts an array and joins the items with comma with "or" after the last one.
-	 *
-	 * @param array $function_list List of alternative functions.
-	 *
-	 * @return string Formatted list of alternative functions.
-	 */
-	private function join_with_comma_and_or( $function_list ) {
-		if ( 1 === count( $function_list ) ) {
-			return $function_list[0];
-		} elseif ( 2 === count( $function_list ) ) {
-			return join( ' or ', $function_list );
-		} else {
-			$last_alternative = array_pop( $function_list );
-			return join( ', ', $function_list ) . ', or ' . $last_alternative;
-		}
-	}
 }
