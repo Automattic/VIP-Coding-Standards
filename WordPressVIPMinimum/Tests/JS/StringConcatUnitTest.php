342eeaf848076bb6aea4bce893bb0c92464ef8da
David Binovec <david.binovec@gmail.com>
2017-12-27 14:30:01 +0000

    Initial pass on HTML string concatenation sniff.
    Per https://vip.wordpress.com/2015/03/25/preventing-xss-in-javascript/

---
+++ WordPressVIPMinimum/Tests/JS/StringConcatUnitTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Unit test class for WordPressVIPMinimum Coding Standard.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Tests\Files;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+/**
+ * Unit test class for the IncludingFile sniff.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+class StringConcatUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array(
+			4 => 2,
+		);
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array();
+
+	}
+
+} // End class.
