7fb93a55fe8e5f2aed5931369777f84861ab73a9
david-binda <david.binovec@gmail.com>
2018-01-19 12:17:34 +0100

    Adding Vue.js unescaped output sniff + unit tests

---
+++ WordPressVIPMinimum/Sniffs/TemplatingEngines/UnescapedOutputVuejsSniff.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * WordPressVIPMinimum_Sniffs_TemplatingEngines_UnescapedOutputTwigSniff.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Sniffs\TemplatingEngines;
+
+use PHP_CodeSniffer_File as File;
+use PHP_CodeSniffer_Tokens as Tokens;
+
+/**
+ * WordPressVIPMinimum_Sniffs_TemplatingEngines_UnescapedOutputVuejsSniff.
+ *
+ * Looks for instances of unescaped output for Twig templating engine.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+class UnescapedOutputVuejsSniff implements \PHP_CodeSniffer_Sniff {
+
+	/**
+	 * A list of tokenizers this sniff supports.
+	 *
+	 * @var array
+	 */
+	public $supportedTokenizers = array(
+		'JS',
+		'PHP',
+	);
+
+	/**
+	 * Returns an array of tokens this test wants to listen for.
+	 *
+	 * @return array
+	 */
+	public function register() {
+		return array(
+			T_CONSTANT_ENCAPSED_STRING,
+			T_INLINE_HTML,
+		);
+
+	}//end register()
+
+	/**
+	 * Processes this test, when one of its tokens is encountered.
+	 *
+	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+	 * @param int                         $stackPtr  The position of the current token in the
+	 *                                               stack passed in $tokens.
+	 *
+	 * @return void
+	 */
+	public function process( File $phpcsFile, $stackPtr ) {
+		$tokens = $phpcsFile->getTokens();
+
+		if ( false !== strpos( $tokens[ $stackPtr ]['content'], 'v-html' ) ) {
+			// Twig autoescape disabled
+			$phpcsFile->addWarning( 'Found Vue.js non-escaped (raw) HTML directive.', $stackPtr, 'v-html' );
+		}
+
+	}//end process()
+
+}//end class
---
+++ WordPressVIPMinimum/Tests/TemplatingEngines/UnescapedOutputVuejsUnitTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Unit test class for WordPressVIPMinimum Coding Standard.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Tests\TemplatingEngines;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the unescaped output in Vue.js templating engine.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+class HTMLExecutingFunctionsUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array();
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array(
+			5  => 1,
+		);
+
+	}
+
+} // End class.
