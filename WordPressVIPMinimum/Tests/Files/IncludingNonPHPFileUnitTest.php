bf90da7ed14d3e81bfc582910759d1f568adaaa9
David Binovec <david.binovec@gmail.com>
2018-04-11 10:00:07 +0000

    Adding tests for non-php file include sniff

---
+++ WordPressVIPMinimum/Tests/Files/IncludingNonPHPFileUnitTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Unit test class for WordPressVIPMinimum Coding Standard.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Tests\Files;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+/**
+ * Unit test class for the IncludingFile sniff.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+class IncludingNonPHPFileUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array(
+			15 => 1,
+			17 => 1,
+			19 => 1,
+			21 => 1,
+			23 => 1,
+			25 => 1,
+			27 => 1,
+			31 => 1,
+			33 => 1,
+			35 => 1,
+			37 => 1,
+			39 => 1,
+			43 => 1,
+			45 => 1,
+			47 => 1,
+			49 => 1,
+		);
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array();
+	}
+
+} // End class.
