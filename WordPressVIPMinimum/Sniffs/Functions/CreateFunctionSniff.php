8013090508290e223f8de3aa4cc8a5981da0d0cc
Gudmundur Haraldsson <gdh1@hi.is>
2018-08-09 17:13:56 +0000

    Altered so not to hit a certain false positive situation. Added test.

--- WordPressVIPMinimum/Sniffs/Functions/CreateFunctionSniff.php
+++ WordPressVIPMinimum/Sniffs/Functions/CreateFunctionSniff.php
@@ -34,50 +34,85 @@ class CreateFunctionSniff implements \PHP_CodeSniffer_Sniff {
 	public function register() {
 		return Tokens::$functionNameTokens;
 
-	}//end register()
+	}
 
 	/**
 	 * Processes the tokens that this sniff is interested in.
 	 *
 	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file where the token was found.
-	 * @param int                         $stackPtr  The position in the stack where
-	 *                                               the token was found.
+	 * @param int $stackPtr The position in the stack where
+	 *                      the token was found.
 	 *
 	 * @return void
 	 */
 	public function process( File $phpcsFile, $stackPtr ) {
-		$this->_tokens    = $phpcsFile->getTokens();
+		$this->_tokens	= $phpcsFile->getTokens();
 		$this->_phpcsFile = $phpcsFile;
 		$this->_stackPtr  = $stackPtr;
 
-		if (
-			( 'T_STRING' !==
-				$this->_tokens[ $this->_stackPtr ]['type'] )
-			||
-			( 'create_function' !==
-				$this->_tokens[ $this->_stackPtr ]['content'] )
-		) {
-			return;
-		}
-
-		$t_item_key = $this->_phpcsFile->findNext(
-			array( T_OPEN_PARENTHESIS ),
-			$this->_stackPtr + 1,
+		$functionName = $phpcsFile->findNext(
+			T_STRING,
+			( $stackPtr ),
 			null,
 			false,
 			null,
 			true
 		);
 
-		if ( false === $t_item_key ) {
-			return;
-		}
+		while ( false !== $functionName ) {
+			if (
+				( 'T_STRING' !==
+					$this->_tokens[ $this->_stackPtr ]['type'] )
+				||
+				( 'create_function' !==
+					$this->_tokens[ $this->_stackPtr ]['content'] )
+			) {
+				// Not a function call, repeat.
+				$functionName = $phpcsFile->findNext(
+					T_STRING,
+					( $functionName + 1 ),
+					null,
+					false,
+					null,
+					true
+				);
 
-		$this->_phpcsFile->addError(
-			'create_function() is deprecated as of ' .
-				'PHP 7.2.0',
-			$t_item_key,
-			'CreateFunction'
-		);
-	}//end process()
+				continue;
+			}
+
+			// Check if this is really a function.
+			$bracket = $phpcsFile->findNext(
+				T_WHITESPACE,
+				( $functionName + 1 ),
+				null,
+				true
+			);
+
+			if (
+				( false === $bracket ) ||
+				( $this->_tokens[ $bracket ]['code'] !== T_OPEN_PARENTHESIS )
+			) {
+				// Not a function call, repeat.
+				$functionName = $phpcsFile->findNext(
+					T_STRING,
+					( $functionName + 1 ),
+					null,
+					false,
+					null,
+					true
+				);
+
+				continue;
+			}
+
+			$this->_phpcsFile->addError(
+				'create_function() is deprecated as of ' .
+					'PHP 7.2.0',
+				$functionName,
+				'CreateFunction'
+			);
+
+			break;
+		}
+	}
 }
