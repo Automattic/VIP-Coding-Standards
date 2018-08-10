ff38a4b36e6bb3bb84894fe028c77e92a274d2c9
Gudmundur Haraldsson <gdh1@hi.is>
2018-08-10 12:07:14 +0000

    Remove stuff that was not needed; focus on T_STRING.

--- WordPressVIPMinimum/Sniffs/Functions/CreateFunctionSniff.php
+++ WordPressVIPMinimum/Sniffs/Functions/CreateFunctionSniff.php
@@ -32,28 +32,8 @@ class CreateFunctionSniff implements \PHP_CodeSniffer_Sniff {
 	 * @return array(int)
 	 */
 	public function register() {
-		return Tokens::$functionNameTokens;
+		return [ T_STRING ];
 
-	}
-
-	/**
-	 * Get next T_STRING token.
-	 *
-	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file where the token was found.
-	 * @param int                         $stackPtr  The position in the stack where
-	 *                                               the token was found.
-	 *
-	 * @return int
-	 */
-	private function getNextTString( File $phpcsFile, $stackPtr ) {
-		return $phpcsFile->findNext(
-			T_STRING,
-			( $stackPtr ),
-			null,
-			false,
-			null,
-			true
-		);
 	}
 
 	/**
@@ -66,54 +46,45 @@ class CreateFunctionSniff implements \PHP_CodeSniffer_Sniff {
 	 * @return void
 	 */
 	public function process( File $phpcsFile, $stackPtr ) {
-		$this->_tokens    = $phpcsFile->getTokens();
-		$this->_phpcsFile = $phpcsFile;
-		$this->_stackPtr  = $stackPtr;
+		$tokens    = $phpcsFile->getTokens();
+		$phpcsFile = $phpcsFile;
+		$stackPtr  = $stackPtr;
 
-		$functionName = $this->getNextTString( $phpcsFile, $stackPtr );
+		$functionName = $phpcsFile->findNext(
+			T_STRING,
+			( $stackPtr ),
+			null,
+			false,
+			null,
+			true
+		);
 
-		while ( false !== $functionName ) {
-			if ( 'create_function' !==
-				$this->_tokens[ $this->_stackPtr ]['content']
-			) {
-				// Not the function call we want.
-				$functionName = $this->getNextTString(
-					$phpcsFile,
-					$functionName + 1
-				);
-
-				continue;
-			}
-
-			// Check if this is really a function.
-			$bracket = $phpcsFile->findNext(
-				T_WHITESPACE,
-				( $functionName + 1 ),
-				null,
-				true
-			);
-
-			if (
-				( false === $bracket ) ||
-				( T_OPEN_PARENTHESIS !== $this->_tokens[ $bracket ]['code'] )
-			) {
-				// Not a function call.
-				$functionName = $this->getNextTString(
-					$phpcsFile,
-					$functionName + 1
-				);
-
-				continue;
-			}
-
-			$this->_phpcsFile->addError(
-				'create_function() is deprecated as of ' .
-					'PHP 7.2.0',
-				$functionName,
-				'CreateFunction'
-			);
-
-			break;
+		if ( 'create_function' !==
+			$tokens[ $stackPtr ]['content']
+		) {
+			return;
 		}
+
+		// Check if this is really a function.
+		$bracket = $phpcsFile->findNext(
+			T_WHITESPACE,
+			( $functionName + 1 ),
+			null,
+			true
+		);
+
+		if (
+			( false === $bracket ) ||
+			( T_OPEN_PARENTHESIS !== $tokens[ $bracket ]['code'] )
+		) {
+			return;
+		}
+
+		$phpcsFile->addError(
+			'create_function() is deprecated as of ' .
+				'PHP 7.2.0',
+			$functionName,
+			'CreateFunction'
+		);
 	}
 }
