3e1b1567d721675ead7c8235ebe6e8d5b646bea8
Rebecca Hum <rebecca.hum@automattic.com>
2018-10-04 13:22:43 -0600

    Initial Commit

---
+++ WordPressVIPMinimum/Sniffs/Filters/RestrictedFilterSniff.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * WordPressVIPMinimum Coding Standard.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Sniffs\Filters;
+
+use PHP_CodeSniffer_File as File;
+use PHP_CodeSniffer_Tokens as Tokens;
+
+/**
+ * This sniff restricts usage of some filters
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+class RestrictedFilterSniff implements \PHP_CodeSniffer_Sniff {
+
+	/**
+	 * List of restricted filter names.
+	 *
+	 * @var array
+	 */
+	public $restrictedFilters = array(
+		'upload_mimes',
+	);
+
+	/**
+	 * Returns the token types that this sniff is interested in.
+	 *
+	 * @return array(int)
+	 */
+	public function register() {
+		return Tokens::$functionNameTokens;
+	}
+
+	/**
+	 * Processes the tokens that this sniff is interested in.
+	 *
+	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file where the token was found.
+	 * @param int                         $stackPtr  The position in the stack where
+	 *                                               the token was found.
+	 *
+	 * @return void
+	 */
+	public function process( File $phpcsFile, $stackPtr ) {
+
+		$tokens = $phpcsFile->getTokens();
+
+		$functionName = $tokens[ $stackPtr ]['content'];
+
+		if ( 'add_filter' !== $functionName ) {
+			return;
+		}
+
+		$filterNamePtr = $phpcsFile->findNext(
+			array_merge( Tokens::$emptyTokens, array( T_OPEN_PARENTHESIS ) ), // types.
+			$stackPtr + 1, // start.
+			null, // end.
+			true, // exclude.
+			null, // value.
+			true // local.
+		);
+
+		$filterName = str_replace( array( "'", '"' ), '', $tokens[ $filterNamePtr ]['content'] );
+
+
+		if ( false === in_array( $filterName, $this->restrictedFilters, true ) ) {
+			// Not the filter we are looking for.
+			return;
+		}
+
+		if ( 'upload_mimes' === $filterName ) {
+			$phpcsFile->addWarning( 'Please ensure that the mime types being filtered do not include SVG. Manual inspection required.', $stackPtr, 'RestrictedFilter' );
+		}
+
+	}
+
+}
