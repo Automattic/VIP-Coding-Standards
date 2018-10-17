7936106089a6d07e111f2aa99e9079de0dd3458f
Rebecca Hum <rebecca.hum@automattic.com>
2018-10-16 22:05:55 -0600

    Made PHPCS changes, added transformString(), privatized $restrictedFilters, checked for concatenated hook name and added more unit tests

--- WordPressVIPMinimum/Sniffs/Filters/RestrictedFilterSniff.php
+++ WordPressVIPMinimum/Sniffs/Filters/RestrictedFilterSniff.php
@@ -11,9 +11,11 @@ use PHP_CodeSniffer_File as File;
 use PHP_CodeSniffer_Tokens as Tokens;
 
 /**
- * This sniff restricts usage of some filters
+ * This sniff restricts usage of some filters.
  *
  * @package VIPCS\WordPressVIPMinimum
+ *
+ * @since 0.4.0
  */
 class RestrictedFilterSniff implements \PHP_CodeSniffer_Sniff {
 
@@ -22,14 +24,14 @@ class RestrictedFilterSniff implements \PHP_CodeSniffer_Sniff {
 	 *
 	 * @var array
 	 */
-	public $restrictedFilters = array(
+	private $restrictedFilters = array(
 		'upload_mimes' => true,
 	);
 
 	/**
 	 * Returns the token types that this sniff is interested in.
 	 *
-	 * @return array(int)
+	 * @return array<int, int>
 	 */
 	public function register() {
 		return Tokens::$functionNameTokens;
@@ -55,18 +57,59 @@ class RestrictedFilterSniff implements \PHP_CodeSniffer_Sniff {
 		}
 
 		$filterNamePtr = $phpcsFile->findNext(
-			array_merge( Tokens::$emptyTokens, array( T_OPEN_PARENTHESIS ) ), // types.
-			$stackPtr + 1, // start.
-			null, // end.
-			true, // exclude.
-			null, // value.
-			true // local.
+			array_merge( Tokens::$emptyTokens, array( T_OPEN_PARENTHESIS ) ),
+			$stackPtr + 1,
+			null,
+			true,
+			null,
+			true
 		);
 
-		$filterName = str_replace( array( "'", '"' ), '', $tokens[ $filterNamePtr ]['content'] );
+		$endHookPtr = $phpcsFile->findNext( 
+			T_COMMA,
+			$filterNamePtr,
+			null,
+			false,
+			null, 
+			true
+		);
+
+		$filterName = $this->transformString( $tokens[ $filterNamePtr ]['content'] );
+
+		// if concatenation is found, build $filterName.
+		$concatPtr = $phpcsFile->findNext(
+			T_STRING_CONCAT,
+			$filterNamePtr,
+			$endHookPtr,
+			false,
+			null, 
+			true
+		);
+		$concatenation = $concatPtr ? true: false; 
+		if ( $concatenation ) { 
+			for ( $i = $filterNamePtr + 1; $i < $endHookPtr; $i++ ) {
+				if ( T_CONSTANT_ENCAPSED_STRING === $tokens[ $i ]['code'] ) {
+					$filterName .= $this->transformString( $tokens[ $i ]['content'] );
+				}
+			}
+		}
 
 		if ( isset( $this->restrictedFilters[ $filterName ] ) && 'upload_mimes' === $filterName ) {
 			$phpcsFile->addWarning( 'Please ensure that the mimes being filtered do not include insecure types (e.g. SVG). Manual inspection required.', $stackPtr, 'UploadMimesRestrictedFilter' );
 		}
+	}
+
+	/**
+	 * Transform string.
+	 *
+	 * @param string $string The name of the filter.
+	 *
+	 * @return string
+	 */
+	private function transformString( $string ) {
+		$string = str_replace( array( "'", '"' ), '', $string ); // remove quotes (double and single).
+		$string = strtolower( $string ); // make sure we don't have weird caps.
+
+		return $string;
 	}
 }
--- WordPressVIPMinimum/Tests/Filters/RestrictedFilterUnitTest.php
+++ WordPressVIPMinimum/Tests/Filters/RestrictedFilterUnitTest.php
@@ -9,9 +9,11 @@ namespace WordPressVIPMinimum\Tests\Filters;
 
 use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
 /**
- * Unit test class for the Filters/AlwaysReturn sniff.
+ * Unit test class for the Filters/RestrictedFilter sniff.
  *
  * @package VIPCS\WordPressVIPMinimum
+ * 
+ * @since 0.4.0
  */
 class RestrictedFilterUnitTest extends AbstractSniffUnitTest {
 
@@ -31,7 +33,14 @@ class RestrictedFilterUnitTest extends AbstractSniffUnitTest {
 	 */
 	public function getWarningList() {
 		return array(
-			5 => 1,
+			6 => 1,
+			7 => 1,
+			8 => 1,
+			9 => 1,
+			10 => 1,
+			11 => 1,
+			12 => 1,
+			13 => 1,
 		);
 	}
 
