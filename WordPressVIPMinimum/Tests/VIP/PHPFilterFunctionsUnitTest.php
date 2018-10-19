5e369d80cde169ad0ec64ce2a005babbfd398dbd
Rebecca Hum <rebecca.hum@automattic.com>
2018-10-19 12:01:26 -0600

    Initial commit

---
+++ WordPressVIPMinimum/Tests/VIP/PHPFilterFunctionsUnitTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Unit test class for WordPressVIPMinimum Coding Standard.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Tests\VIP;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the WP_Query params sniff.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+class PHPFilterFunctionsUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array();
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array(
+			16 => 1,
+			17 => 1,
+			18 => 1,
+			19 => 1,
+			20 => 1,
+			21 => 1,
+			22 => 1,
+			23 => 1,
+			24 => 1,
+		);
+	}
+
+}
