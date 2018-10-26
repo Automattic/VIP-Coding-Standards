7de08dd52bfbe3d2c0190974ff20d0f4a77f95a4
Rebecca Hum <rebecca.hum@automattic.com>
2018-10-25 22:56:03 -0600

    Add Sniff for WP_CLI_Command, related unit tests and to ruleset.

---
+++ WordPressVIPMinimum/Sniffs/Classes/RestrictedClassesSniff.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * WordPressVIPMinimum Coding Standard.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Sniffs\Classes;
+
+use WordPress\AbstractClassRestrictionsSniff;
+
+use PHP_CodeSniffer\Util\Tokens;
+
+/**
+ * WordPressVIPMinimum_Sniffs_Classes_RestrictedClassesSniff.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ *
+ * @since 0.4.0
+ */
+class RestrictedClassesSniff extends AbstractClassRestrictionsSniff {
+
+	public function getGroups() {
+		return array(
+			'wp_cli' => array(
+				'classes' => array(
+					'WP_CLI_Command',
+				),
+			),
+		);
+	}
+
+	/**
+	 * Process a matched token.
+	 *
+	 * @param int    $stackPtr        The position of the current token in the stack.
+	 * @param array  $group_name      The name of the group which was matched. 
+	 * @param string $matched_content The token content (class name) which was matched.
+	 *
+	 * @return void
+	 */
+	public function process_matched_token( $stackPtr, $group_name, $matched_content ) {
+		$tokens = $this->phpcsFile->getTokens();
+
+		if ( T_EXTENDS === $tokens[ $stackPtr ]['code'] ) {
+			$this->phpcsFile->addWarning( 'We recommend extending `WPCOM_VIP_CLI_Command` instead of `WP_CLI_Command` and using the helper functions available in it (such as `stop_the_insanity()`), see https://vip.wordpress.com/documentation/writing-bin-scripts/ for more information.', $stackPtr, 'Extend_WP_CLI_Command' );
+		}
+	}
+}
---
+++ WordPressVIPMinimum/Tests/Classes/RestrictedClassesUnitTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Unit test class for WordPressVIPMinimum Coding Standard.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+
+namespace WordPressVIPMinimum\Tests\Classes;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the DeclarationCompatibility sniff.
+ *
+ * @package VIPCS\WordPressVIPMinimum
+ */
+class RestrictedClassesUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return [];
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return [ 
+			8  => 1,
+			9  => 1,
+			10 => 1,
+			11 => 1,
+		];
+	}
+
+}
