7a9362119abf4be14e41c6084bf826a3b998ce8e
Rebecca Hum <rebecca.hum@automattic.com>
2018-10-26 18:22:42 -0600

    Refactor WordPressVIPMinimum.RestrictedFunctions and remove wpcom_vip_get_page_by_path

--- WordPressVIPMinimum/Sniffs/VIP/RestrictedFunctionsSniff.php
+++ WordPressVIPMinimum/Sniffs/VIP/RestrictedFunctionsSniff.php
@@ -7,12 +7,15 @@
 
 namespace WordPressVIPMinimum\Sniffs\VIP;
 
+use WordPress\AbstractFunctionRestrictionsSniff;
+use WordPress\AbstractFunctionRestrictionSniff;
+
 /**
  * Restricts usage of some functions in VIP context.
  *
  * @package VIPCS\WordPressVIPMinimum
  */
-class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctionsSniff {
+class RestrictedFunctionsSniff extends AbstractFunctionRestrictionsSniff {
 
 	/**
 	 * Groups of functions to restrict.
@@ -21,9 +24,7 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 	 */
 	public function getGroups() {
 
-		$original_groups = parent::getGroups();
-
-		$new_groups = array(
+		$groups = array(
 			'wp_cache_get_multi'       => array(
 				'type'      => 'error',
 				'message'   => '`%s` is not supported on the WordPress.com VIP platform.',
@@ -50,7 +51,9 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 			'get_super_admins'         => array(
 				'type'      => 'error',
 				'message'   => '`%s` is prohibited on the WordPress.com VIP platform',
-				'functions' => array( 'get_super_admins' ),
+				'functions' => array( 
+					'get_super_admins' 
+				),
 			),
 			'internal'                 => array(
 				'type'      => 'error',
@@ -59,6 +62,7 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 					'wpcom_vip_irc',
 				),
 			),
+			// @link WordPress.com: https://lobby.vip.wordpress.com/wordpress-com-documentation/code-review-what-we-look-for/#flush_rewrite_rules
 			'rewrite_rules'            => array(
 				'type'      => 'error',
 				'message'   => '`%s` should not be used in any normal circumstances in the theme code.',
@@ -73,11 +77,109 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 					'attachment_url_to_postid',
 				),
 			),
+			// TODO: Add strip_tags sniff that checks based on parameters.
 			'strip_tags'               => array(
 				'type'      => 'error',
 				'message'   => '`%s()` does not strip CSS and JS in between the script and style tags. `wp_strip_all_tags()` should be used instead.',
 				'functions' => array(
 					'strip_tags',
+				),
+			),
+			'dbDelta'                  => array(
+				'type'      => 'error',
+				'message'   => 'All database modifications have to approved by the WordPress.com VIP team.',
+				'functions' => array(
+					'dbDelta',
+				),
+			),
+			// @link WordPress.com: https://vip.wordpress.com/documentation/vip/code-review-what-we-look-for/#switch_to_blog
+			// @link VIP Go: https://vip.wordpress.com/documentation/vip-go/code-review-blockers-warnings-notices/#switch_to_blog
+			'switch_to_blog' => array(
+				'type'      => 'error',
+				'message'   => '%s() is not something you should ever need to do in a VIP theme context. Instead use an API (XML-RPC, REST) to interact with other sites if needed.',
+				'functions' => array( 'switch_to_blog' ),
+			),
+			'get_page_by_title' => array(
+				'type'      => 'error',
+				'message'   => '%s() is prohibited, please use wpcom_vip_get_page_by_title() instead.',
+				'functions' => array(
+					'get_page_by_title',
+				),
+			),
+			'url_to_postid' => array(
+				'type'      => 'error',
+				'message'   => '%s() is prohibited, please use wpcom_vip_url_to_postid() instead.',
+				'functions' => array(
+					'url_to_postid',
+					'url_to_post_id',
+				),
+			),
+			// @link WordPress.com: https://lobby.vip.wordpress.com/wordpress-com-documentation/code-review-what-we-look-for/#custom-roles
+			// @link VIP Go: https://vip.wordpress.com/documentation/vip-go/code-review-blockers-warnings-notices/#custom-roles
+			'custom_role' => array(
+				'type'      => 'error',
+				'message'   => 'Use wpcom_vip_add_role() instead of %s()',
+				'functions' => array(
+					'add_role',
+				),
+			),
+			// @link WordPress.com: https://lobby.vip.wordpress.com/wordpress-com-documentation/code-review-what-we-look-for/#wp_users-and-user_meta
+			'user_meta' => array(
+				'type'      => 'error',
+				'message'   => '%s() usage is highly discouraged on WordPress.com VIP due to it being a multisite, please see https://lobby.vip.wordpress.com/wordpress-com-documentation/code-review-what-we-look-for/#wp_users-and-user_meta.',
+				'functions' => array(
+					'get_user_meta',
+					'update_user_meta',
+					'delete_user_meta',
+					'add_user_meta',
+				),
+			),
+			'term_exists' => array(
+				'type'      => 'error',
+				'message'   => '%s() is highly discouraged due to not being cached; please use wpcom_vip_term_exists() instead.',
+				'functions' => array(
+					'term_exists',
+				),
+			),
+			'count_user_posts' => array(
+				'type'      => 'error',
+				'message'   => '%s() is highly discouraged due to not being cached; please use wpcom_vip_count_user_posts() instead.',
+				'functions' => array(
+					'count_user_posts',
+				),
+			),
+			'wp_old_slug_redirect' => array(
+				'type'      => 'error',
+				'message'   => '%s() is highly discouraged due to not being cached; please use wpcom_vip_old_slug_redirect() instead.',
+				'functions' => array(
+					'wp_old_slug_redirect',
+				),
+			),
+			'get_adjacent_post' => array(
+				'type'      => 'error',
+				'message'   => '%s() is highly discouraged due to not being cached; please use wpcom_vip_get_adjacent_post() instead.',
+				'functions' => array(
+					'get_adjacent_post',
+					'get_previous_post',
+					'get_previous_post_link',
+					'get_next_post',
+					'get_next_post_link',
+				),
+			),
+			'get_intermediate_image_sizes' => array(
+				'type'      => 'error',
+				'message'   => 'Intermediate images do not exist on the VIP platform, and thus get_intermediate_image_sizes() returns an empty array() on the platform. This behavior is intentional to prevent WordPress from generating multiple thumbnails when images are uploaded.',
+				'functions' => array(
+					'get_intermediate_image_sizes',
+				),
+			),
+			// @link WordPress.com: https://lobby.vip.wordpress.com/wordpress-com-documentation/code-review-what-we-look-for/#mobile-detection
+			// @link VIP Go: https://vip.wordpress.com/documentation/vip-go/code-review-blockers-warnings-notices/#mobile-detection
+			'wp_is_mobile' => array(
+				'type'      => 'error',
+				'message'   => '%s() found. When targeting mobile visitors, jetpack_is_mobile() should be used instead of wp_is_mobile. It is more robust and works better with full page caching.',
+				'functions' => array(
+					'wp_is_mobile',
 				),
 			),
 			'wp_mail'                  => array(
@@ -86,13 +188,6 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 				'functions' => array(
 					'wp_mail',
 					'mail',
-				),
-			),
-			'dbDelta'                  => array(
-				'type'      => 'error',
-				'message'   => 'All database modifications have to approved by the WordPress.com VIP team.',
-				'functions' => array(
-					'dbDelta',
 				),
 			),
 			'is_multi_author'          => array(
@@ -110,6 +205,34 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 					'the_field',
 				),
 			),
+			// @link WordPress.com: https://lobby.vip.wordpress.com/wordpress-com-documentation/code-review-what-we-look-for/#remote-calls
+			// @link VIP Go: https://vip.wordpress.com/documentation/vip-go/code-review-blockers-warnings-notices/#remote-calls
+			'wp_remote_get' => array(
+				'type'      => 'warning',
+				'message'   => '%s() is highly discouraged, please use vip_safe_wp_remote_get() instead.',
+				'functions' => array(
+					'wp_remote_get',
+				),
+			),
+			// @link WordPress.com: https://lobby.vip.wordpress.com/wordpress-com-documentation/code-review-what-we-look-for/#custom-roles
+			// @link VIP Go: https://vip.wordpress.com/documentation/vip-go/code-review-blockers-warnings-notices/#cache-constraints
+			'cookies' => array(
+				'type'      => 'warning',
+				'message'   => 'Due to using Batcache, server side based client related logic will not work, use JS instead.',
+				'functions' => array(
+					'setcookie',
+				),
+			),
+			// @todo Introduce a sniff specific to get_posts() that checks for suppress_filters=>false being supplied.
+			'get_posts' => array(
+				'type'      => 'warning',
+				'message'   => '%s() is uncached unless the "suppress_filters" parameter is set to false. If the suppress_filter parameter is set to false this can be safely ignored. More Info: https://vip.wordpress.com/documentation/vip-go/uncached-functions/',
+				'functions' => array(
+					'get_posts',
+					'wp_get_recent_posts',
+					'get_children',
+				),
+			),
 		);
 
 		$deprecated_vip_helpers = array(
@@ -118,18 +241,16 @@ class RestrictedFunctionsSniff extends \WordPress\Sniffs\VIP\RestrictedFunctions
 			'get_category_by_slug' => 'wpcom_vip_get_category_by_slug',
 		);
 		foreach ( $deprecated_vip_helpers as $restricted => $helper ) {
-			$new_groups[ $helper ] = array(
+			$groups[ $helper ] = array(
 				'type'      => 'warning',
-				'message'   => "`%s()` is deprecated, please use {$restricted} instead.",
+				'message'   => "`%s()` is deprecated, please use `{$restricted}()` instead.",
 				'functions' => array(
 					$helper,
 				),
 			);
-			unset( $original_groups[ $restricted ] );
+			unset( $groups[ $restricted ] );
 		}
 
-		unset( $original_groups['file_get_contents'] );
-
-		return array_merge( $original_groups, $new_groups );
+		return $groups;
 	}
 }
--- WordPressVIPMinimum/Tests/VIP/RestrictedFunctionsUnitTest.php
+++ WordPressVIPMinimum/Tests/VIP/RestrictedFunctionsUnitTest.php
@@ -22,22 +22,42 @@ class RestrictedFunctionsUnitTest extends AbstractSniffUnitTest {
 	 * @return array <int line number> => <int number of errors>
 	 */
 	public function getErrorList() {
-		return array(
-			3  => 1,
-			7  => 1,
-			9  => 1,
-			11 => 1,
-			13 => 1,
-			39 => 1,
-			51 => 1,
-			52 => 1,
-			53 => 1,
-			54 => 1,
-			55 => 1,
-			56 => 1,
-			57 => 1,
-			58 => 1,
-		);
+		return [
+			19  => 1,
+			27  => 1,
+			28  => 1,
+			29  => 1,
+			30  => 1,
+			31  => 1,
+			32  => 1,
+			33  => 1,
+			34  => 1,
+			37  => 1,
+			40  => 1,
+			43  => 1,
+			46  => 1,
+			50  => 1,
+			53  => 1,
+			56  => 1,
+			59  => 1,
+			62  => 1,
+			75  => 1,
+			76  => 1,
+			82  => 1,
+			83  => 1,
+			84  => 1,
+			85  => 1,
+			88  => 1,
+			91  => 1,
+			94  => 1,
+			97  => 1,
+			98  => 1,
+			99  => 1,
+			100 => 1,
+			101 => 1,
+			104 => 1,
+			107 => 1,
+		];
 	}
 
 	/**
@@ -46,18 +66,21 @@ class RestrictedFunctionsUnitTest extends AbstractSniffUnitTest {
 	 * @return array <int line number> => <int number of warnings>
 	 */
 	public function getWarningList() {
-		return array(
-			1  => 1,
-			5  => 1,
-			29 => 1,
-			31 => 1,
-			33 => 1,
-			35 => 1,
-			37 => 1,
-			41 => 1,
-			43 => 1,
-			44 => 1,
-		);
+		return [
+			110 => 1,
+			111 => 1,
+			114 => 1,
+			118 => 1,
+			119 => 1,
+			122 => 1,
+			125 => 1,
+			130 => 1,
+			131 => 1,
+			132 => 1,
+			137 => 1,
+			138 => 1,
+			139 => 1,
+		];
 	}
 
 }
