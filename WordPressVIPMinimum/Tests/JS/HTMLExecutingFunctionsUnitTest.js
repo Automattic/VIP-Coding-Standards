4a8e464a664cfdcc1ebd6c9cac6b754d592e0b4f
Gary Jones <gary.jones@automattic.com>
2019-08-18 13:35:42 +0100

    HTMLExecutingFunctions: Add more functions
    There are different ways to insert variables into the DOM, and our sniffs only covered some of them.
    
    The sniff now has an expanded list, which covers functions that have a syntax where the content is in the function arg, and also where the function arg is the target and the content is in the preceding method's arg.
    
    Fixes #435.

--- WordPressVIPMinimum/Tests/JS/HTMLExecutingFunctionsUnitTest.js
+++ WordPressVIPMinimum/Tests/JS/HTMLExecutingFunctionsUnitTest.js
@@ -1,13 +1,44 @@
 (function(){
     var el = document.querySelector(".myclass");
-    el.html(''); // OK.
-    el.html('<b>Hand written HTML</b>'); // OK.
-    el.html( '<b>' + variable + '</b>' ); // NOK.
-    el.html( variable ); // NOK.
-    el.append( variable ); // NOK.
+	el.after(''); // OK.
+	el.after('<b>Hand written HTML</b>'); // OK.
+	el.after( '<b>' + variable + '</b>' ); // Warning.
+	el.after( variable ); // Warning.
     el.append( '<b>Hand written HTML</b>' ); // OK.
-    el.append( '<b>' + variable + '</b>' ); // NOK.
+    el.append( '<b>' + variable + '</b>' ); // Warning.
+    el.append( variable ); // Warning.
+	el.before('<b>Hand written HTML</b>'); // OK.
+	el.before( '<b>' + variable + '</b>' ); // Warning.
+	el.before( variable ); // Warning.
+    el.html('<b>Hand written HTML</b>'); // OK.
+    el.html( '<b>' + variable + '</b>' ); // Warning.
+    el.html( variable ); // Warning.
+	el.prepend('<b>Hand written HTML</b>'); // OK.
+	el.prepend( '<b>' + variable + '</b>' ); // Warning.
+	el.prepend( variable ); // Warning.
+	el.replaceWith('<b>Hand written HTML</b>'); // OK.
+	el.replaceWith( '<b>' + variable + '</b>' ); // Warning.
+	el.replaceWith( variable ); // Warning.
     document.write( '<script>console.log("hello")</script>' ); // OK. No variable, conscious.
-    document.write( hello ); // NOK.
-    document.writeln( hey ); // NOK.
+    document.write( hello ); // Warning.
+    document
+		.	writeln( hey ); // Warning.
+
+	$('').appendTo(el); // OK.
+	$('<b>Hand written HTML</b>').appendTo( el ); // OK.
+	$( '<b>' + variable + '</b>' ).appendTo( el ); // Warning.
+	$( variable ).appendTo( el ); // Warning.
+	$('<b>Hand written HTML</b>').insertAfter( el ); // OK.
+	$( '<b>' + variable + '</b>' ).insertAfter( el ); // Warning.
+	$( variable ).insertAfter( el ); // Warning.
+	$('<b>Hand written HTML</b>').insertBefore( el ); // OK.
+	$( '<b>' + variable + '</b>' ).insertBefore( el ); // Warning.
+	$( variable ).insertBefore( el ); // Warning.
+	$('<b>Hand written HTML</b>').prependTo( el ); // OK.
+	$( '<b>' + variable + '</b>' ).prependTo( el ); // Warning.
+	$( variable ).prependTo( el ); // Warning.
+	$('<b>Hand written HTML</b>').replaceAll( el ); // OK.
+	$( '<b>' + variable + '</b>' ).replaceAll( el ); // Warning.
+	$( variable )
+		.		replaceAll( el ); // Warning.
 })();
