b1b0393a2eb5883d8ecb1ca28456871cfcfc478b
Rebecca Hum <rebecca.hum@automattic.com>
2019-08-29 17:44:50 -0600

    LowExpiryCacheTimeSniff: Account for random generating number functions used as cache expiry time

--- WordPressVIPMinimum/Tests/Performance/LowExpiryCacheTimeUnitTest.php
+++ WordPressVIPMinimum/Tests/Performance/LowExpiryCacheTimeUnitTest.php
@@ -32,10 +32,6 @@ class LowExpiryCacheTimeUnitTest extends AbstractSniffUnitTest {
 	 */
 	public function getWarningList() {
 		return [
-			27 => 1,
-			28 => 1,
-			29 => 1,
-			30 => 1,
 			32 => 1,
 			33 => 1,
 			34 => 1,
@@ -44,6 +40,14 @@ class LowExpiryCacheTimeUnitTest extends AbstractSniffUnitTest {
 			38 => 1,
 			39 => 1,
 			40 => 1,
+			42 => 1,
+			43 => 1,
+			44 => 1,
+			45 => 1,
+			47 => 1,
+			48 => 1,
+			49 => 1,
+			50 => 1,
 		];
 	}
 }
