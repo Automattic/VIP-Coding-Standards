0faf12fad8736e80a2fa8d43934855741d0d83fb
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2020-02-03 12:45:34 -0700

    IncludingFileSniff: Add get_parent_theme_file_path to safelist

--- WordPressVIPMinimum/Sniffs/Files/IncludingFileSniff.php
+++ WordPressVIPMinimum/Sniffs/Files/IncludingFileSniff.php
@@ -31,7 +31,7 @@ class IncludingFileSniff extends AbstractFunctionRestrictionsSniff {
 		'get_stylesheet_directory',
 		'get_template_directory',
 		'locate_template',
-		'get_parent_theme_file_path'
+		'get_parent_theme_file_path',
 	];
 
 	/**
