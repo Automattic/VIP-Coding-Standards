b57c9fdae4e03ebc5deea338b6e16fbd381fd4bc
Gary Jones <gary.jones@automattic.com>
2022-12-20 11:31:55 +0000

    Ruleset tests: Add label before test runs
    Make it clear which ruleset being tested, even on a test failure.

--- tests/RulesetTest.php
+++ tests/RulesetTest.php
@@ -98,6 +98,9 @@ class RulesetTest {
 			$this->phpcs_bin = realpath( $phpcs_bin );
 		}
 
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		printf( 'Testing the ' . $this->ruleset . ' ruleset.' . PHP_EOL );
+
 		$output = $this->collect_phpcs_result();
 
 		if ( ! is_object( $output ) || empty( $output ) ) {
@@ -144,10 +147,11 @@ class RulesetTest {
 
 		$shell = sprintf(
 			'%1$s%2$s --severity=1 --standard=%3$s --report=json ./%3$s/ruleset-test.inc',
-			$php, // Current PHP executable if avaiable.
+			$php, // Current PHP executable if available.
 			$this->phpcs_bin,
 			$this->ruleset
 		);
+
 		// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_shell_exec -- This is test code, not production.
 		$output = shell_exec( $shell );
 
