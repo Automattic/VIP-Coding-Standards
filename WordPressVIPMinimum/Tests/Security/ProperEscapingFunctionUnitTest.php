3c5a8bb0fa32cccf9518aeda8dc8c1e39e7add5d
Gary Jones <gary.jones@automattic.com>
2023-02-05 17:14:29 +0000

    ProperEscapingFunction: Prep for multi-file tests
    Renames the singular test `.inc` file to include a number, to allow for more incoming test files.

--- WordPressVIPMinimum/Tests/Security/ProperEscapingFunctionUnitTest.php
+++ WordPressVIPMinimum/Tests/Security/ProperEscapingFunctionUnitTest.php
@@ -21,43 +21,51 @@ class ProperEscapingFunctionUnitTest extends AbstractSniffUnitTest {
 	/**
 	 * Returns the lines where errors should occur.
 	 *
+	 * @param string $testFile The name of the file being tested.
+	 *
 	 * @return array <int line number> => <int number of errors>
 	 */
-	public function getErrorList() {
-		return [
-			3   => 1,
-			5   => 1,
-			15  => 1,
-			17  => 1,
-			21  => 1,
-			23  => 1,
-			33  => 1,
-			37  => 1,
-			41  => 1,
-			45  => 1,
-			48  => 1,
-			62  => 1,
-			63  => 1,
-			64  => 1,
-			65  => 1,
-			67  => 1,
-			68  => 1,
-			69  => 1,
-			72  => 1,
-			73  => 1,
-			74  => 1,
-			75  => 1,
-			76  => 1,
-			77  => 1,
-			78  => 1,
-			79  => 1,
-			80  => 1,
-			82  => 1,
-			92  => 1,
-			97  => 1,
-			102 => 1,
-			104 => 1,
-		];
+	public function getErrorList( $testFile = '' ) {
+		switch ( $testFile ) {
+			case 'ProperEscapingFunctionUnitTest.1.inc':
+				return [
+					3   => 1,
+					5   => 1,
+					15  => 1,
+					17  => 1,
+					21  => 1,
+					23  => 1,
+					33  => 1,
+					37  => 1,
+					41  => 1,
+					45  => 1,
+					48  => 1,
+					62  => 1,
+					63  => 1,
+					64  => 1,
+					65  => 1,
+					67  => 1,
+					68  => 1,
+					69  => 1,
+					72  => 1,
+					73  => 1,
+					74  => 1,
+					75  => 1,
+					76  => 1,
+					77  => 1,
+					78  => 1,
+					79  => 1,
+					80  => 1,
+					82  => 1,
+					92  => 1,
+					97  => 1,
+					102 => 1,
+					104 => 1,
+				];
+
+			default:
+				return [];
+		}
 	}
 
 	/**
