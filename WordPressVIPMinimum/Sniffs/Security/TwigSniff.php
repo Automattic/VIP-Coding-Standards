b10aaeba84a1f7153822bb1ed3303e3d42cc5965
jrfnl <jrfnl@users.noreply.github.com>
2025-07-16 16:15:40 +0200

    Security/Twig: bug fix - prevent false positives on interpolated expressions
    Double quoted strings and heredocs may contain interpolated expressions, which could lead to false positives.
    
    Fixed now.
    
    Includes unit tests proving the bug.

--- WordPressVIPMinimum/Sniffs/Security/TwigSniff.php
+++ WordPressVIPMinimum/Sniffs/Security/TwigSniff.php
@@ -9,6 +9,7 @@
 namespace WordPressVIPMinimum\Sniffs\Security;
 
 use PHP_CodeSniffer\Util\Tokens;
+use PHPCSUtils\Utils\TextStrings;
 use WordPressVIPMinimum\Sniffs\Sniff;
 
 /**
@@ -40,14 +41,21 @@ class TwigSniff extends Sniff {
 	 * @return void
 	 */
 	public function process_token( $stackPtr ) {
+		// Strip any potentially interpolated expressions.
+		$only_text = $this->tokens[ $stackPtr ]['content'];
+		if ( $this->tokens[ $stackPtr ]['code'] === T_DOUBLE_QUOTED_STRING
+			|| $this->tokens[ $stackPtr ]['code'] === T_HEREDOC
+		) {
+			$only_text = TextStrings::stripEmbeds( $only_text );
+		}
 
-		if ( preg_match( '/autoescape\s+false/', $this->tokens[ $stackPtr ]['content'] ) === 1 ) {
+		if ( preg_match( '/autoescape\s+false/', $only_text ) === 1 ) {
 			// Twig autoescape disabled.
 			$message = 'Found Twig autoescape disabling notation.';
 			$this->phpcsFile->addWarning( $message, $stackPtr, 'AutoescapeFalse' );
 		}
 
-		if ( preg_match( '/\|\s*raw/', $this->tokens[ $stackPtr ]['content'] ) === 1 ) {
+		if ( preg_match( '/\|\s*raw/', $only_text ) === 1 ) {
 			// Twig default unescape filter.
 			$message = 'Found Twig default unescape filter: "|raw".';
 			$this->phpcsFile->addWarning( $message, $stackPtr, 'RawFound' );
