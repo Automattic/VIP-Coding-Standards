174a08bfdfc7a3d11ccb1fcaaf22ab1ad10dfd79
jrfnl <jrfnl@users.noreply.github.com>
2025-07-17 18:57:33 +0200

    Functions/StripTags: improve the tests
    * Test against false positives for calls to methods or namespaced function calls.
    * Test against false positives for attribute class using the same name as the function.
    * Ensure function import `use` statements are not flagged. We're not interested in those.
    * Add some more variations to the pre-existing tests:
        - Non-lowercase function call(s).
        - Fully qualified function calls.
        - Use PHP 7.3+ trailing comma's in a few function calls.
    
    Includes cleaning up some "Warning" comments which contained irrelevant information as the sniff isn't looking for the things which were previously mentioned.

--- WordPressVIPMinimum/Tests/Functions/StripTagsUnitTest.php
+++ WordPressVIPMinimum/Tests/Functions/StripTagsUnitTest.php
@@ -32,11 +32,11 @@ class StripTagsUnitTest extends AbstractSniffUnitTest {
 	 */
 	public function getWarningList() {
 		return [
-			9  => 1,
-			10 => 1,
-			11 => 1,
-			12 => 1,
-			13 => 1,
+			29 => 1,
+			30 => 1,
+			31 => 1,
+			32 => 1,
+			33 => 1,
 		];
 	}
 }
