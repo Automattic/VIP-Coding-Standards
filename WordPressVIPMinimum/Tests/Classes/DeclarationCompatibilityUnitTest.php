e9ac45fbad75f610d97679d1c1006858d57ca46a
jrfnl <jrfnl@users.noreply.github.com>
2025-07-24 23:03:25 +0200

    Classes/DeclarationCompatibility: improve the tests
    Completely rewrite and greatly expand the tests to ensure a significantly larger portion of what the sniff checks is actually tested.

--- WordPressVIPMinimum/Tests/Classes/DeclarationCompatibilityUnitTest.php
+++ WordPressVIPMinimum/Tests/Classes/DeclarationCompatibilityUnitTest.php
@@ -19,36 +19,95 @@ class DeclarationCompatibilityUnitTest extends AbstractSniffUnitTest {
 	/**
 	 * Returns the lines where errors should occur.
 	 *
+	 * @param string $testFile The name of the file being tested.
+	 *
 	 * @return array<int, int> Key is the line number, value is the number of expected errors.
 	 */
-	public function getErrorList() {
-		return [
-			4   => 1,
-			7   => 1,
-			10  => 1,
-			13  => 1,
-			16  => 1,
-			19  => 1,
-			25  => 1,
-			40  => 1,
-			43  => 1,
-			46  => 1,
-			49  => 1,
-			52  => 1,
-			61  => 1,
-			67  => 1,
-			70  => 1,
-			76  => 1,
-			79  => 1,
-			88  => 1,
-			106 => 1,
-			112 => 1,
-			119 => 1,
-			128 => 1,
-			134 => 1,
-			137 => 1,
-			140 => 1,
-		];
+	public function getErrorList( $testFile = '' ) {
+		switch ( $testFile ) {
+			case 'DeclarationCompatibilityUnitTest.1.inc':
+				return [
+					53  => 1,
+					54  => 1,
+					55  => 1,
+					56  => 1,
+					57  => 1,
+					58  => 1,
+					59  => 1,
+					60  => 1,
+					61  => 1,
+					62  => 1,
+					63  => 1,
+					64  => 1,
+					68  => 1,
+					69  => 1,
+					70  => 1,
+					71  => 1,
+					75  => 1,
+					76  => 1,
+					77  => 1,
+					78  => 1,
+					79  => 1,
+					80  => 1,
+					81  => 1,
+					82  => 1,
+					83  => 1,
+					84  => 1,
+					85  => 1,
+					86  => 1,
+					87  => 1,
+					88  => 1,
+					89  => 1,
+					90  => 1,
+					91  => 1,
+					92  => 1,
+					96  => 1,
+					97  => 1,
+					98  => 1,
+					99  => 1,
+					100 => 1,
+				];
+
+			case 'DeclarationCompatibilityUnitTest.2.inc':
+				return [
+					43 => 1,
+					44 => 1,
+					45 => 1,
+					46 => 1,
+					47 => 1,
+					48 => 1,
+					49 => 1,
+					50 => 1,
+					51 => 1,
+					55 => 1,
+					56 => 1,
+					57 => 1,
+					58 => 1,
+					59 => 1,
+					60 => 1,
+					61 => 1,
+					62 => 1,
+					66 => 1,
+					67 => 1,
+					68 => 1,
+					72 => 1,
+					73 => 1,
+					74 => 1,
+					75 => 1,
+					79 => 1,
+					80 => 1,
+					81 => 1,
+					82 => 1,
+					83 => 1,
+					84 => 1,
+					88 => 1,
+					92 => 1,
+					93 => 1,
+				];
+
+			default:
+				return [];
+		}
 	}
 
 	/**
